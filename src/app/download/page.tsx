7f4379da22af4c2354f15aedcecc0b18a054f9f9
GreemDev <greemdev@ryujinx.app>
2026-05-12 03:44:30 -0500

    Add the ability to download canaries by using `/download?canary`

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -15,7 +15,7 @@ import {
 import {downloads, baseURL} from "@/resources";
 import Consts from "@/utils";
 import {DownloadButton} from "@/components/DownloadButtons";
-import {formatSupportedCPU} from "@/utils/downloads";
+import {formatSupportedCPU, ReleaseChannel} from "@/utils/downloads";
 
 export async function generateMetadata() {
     return Meta.generate({
@@ -27,7 +27,29 @@ export async function generateMetadata() {
     });
 }
 
-export default function Downloads() {
+export default async function Downloads(
+    {
+        searchParams,
+    }: {
+        searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+    }
+) {
+    const sp = (await searchParams);
+
+    console.log(JSON.stringify(sp))
+
+    let rawRc = sp.rc
+
+    if (rawRc === undefined) {
+        if (sp.canary !== undefined) {
+            rawRc = "canary"
+        }
+    }
+
+    const rc: ReleaseChannel = typeof rawRc !== "string"
+        ? "stable"
+        : rawRc.toLowerCase() as ReleaseChannel
+
     return (
         <Column maxWidth="m" gap="xl" paddingY="12" horizontal="center">
             <Schema
@@ -74,7 +96,7 @@ export default function Downloads() {
                                     </Row>
                                     <Line background="neutral-alpha-medium"/>
                                     <Row horizontal="center" padding="m" gap="l">
-                                        <DownloadButton platform="win" cpu="x64"/>
+                                        <DownloadButton platform="win" cpu="x64" rc={rc}/>
                                     </Row>
                                 </Card>
                                 <Card radius="l-4" direction="column" border="brand-strong" horizontal="center">
@@ -90,7 +112,7 @@ export default function Downloads() {
                                     </Row>
                                     <Line background="neutral-alpha-medium"/>
                                     <Row horizontal="center" padding="m" gap="l">
-                                        <DownloadButton platform="mac" cpu="arm64"
+                                        <DownloadButton platform="mac" cpu="arm64" rc={rc}
                                                         getLabel="Mac (Apple Silicon)"/>
                                     </Row>
                                 </Card>
@@ -103,23 +125,25 @@ export default function Downloads() {
                                             <Heading>Linux</Heading>
                                             <Line background="neutral-alpha-medium"/>
                                             <Text onBackground="neutral-weak" variant="body-default-xl">
-                                                We have tarballs, including a standard Linux executable with all of the dependencies needed to run it included.<br/>
-                                                Additionally, we offer AppImages, of both architectures, for those who wish to use it.
+                                                We have tarballs, including a standard Linux executable with all of the
+                                                dependencies needed to run it included.<br/>
+                                                Additionally, we offer AppImages, of both architectures, for those who
+                                                wish to use it.
                                             </Text>
                                         </Column>
                                     </Row>
                                     <Line background="neutral-alpha-medium"/>
                                     <Column gap="s" padding="m">
                                         <Row horizontal="center" gap="m">
-                                            <DownloadButton platform="linux" cpu="x64"
+                                            <DownloadButton platform="linux" cpu="x64" rc={rc}
                                                             getLabel={(_, cpu, ___) => `Tarball: ${formatSupportedCPU(cpu)}`}/>
-                                            <DownloadButton platform="linux" cpu="arm64"
+                                            <DownloadButton platform="linux" cpu="arm64" rc={rc}
                                                             getLabel={(_, cpu, ___) => `Tarball: ${formatSupportedCPU(cpu)}`}/>
                                         </Row>
                                         <Row horizontal="center" gap="m">
-                                            <DownloadButton platform="linuxai" cpu="x64"
+                                            <DownloadButton platform="linuxai" cpu="x64" rc={rc}
                                                             getLabel={(_, cpu, ___) => `AppImage: ${formatSupportedCPU(cpu)}`}/>
-                                            <DownloadButton platform="linuxai" cpu="arm64"
+                                            <DownloadButton platform="linuxai" cpu="arm64" rc={rc}
                                                             getLabel={(_, cpu, ___) => `AppImage: ${formatSupportedCPU(cpu)}`}/>
                                         </Row>
                                     </Column>
